9c36012d60d9b7316627f46ff7c7bc3f70a1f438
João Oliveira <jaoovit@gmail.com>
2024-05-15 12:46:15 +0100

    recreate ship tests

--- src/ship.js
+++ src/ship.js
@@ -7,21 +7,43 @@ class Ship {
 
   hit() {
     this.hits++;
-    return this;
+    return this.hits;
   }
 
   isSunk() {
     if (this.hits >= this.length) {
       this.sunk = true;
+      this.sunk;
     }
-    return this;
+    return this.sunk;
+  }
+}
+/*
+class Gameboard {
+  creteBoard() {
+    let board = new Array();
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        board.push([i, j]);
+      }
+    }
+    return board;
+  }
+
+  constructor() {
+    this.board = this.creteBoard();
+    this.enemyBoard = this.creteBoard();
   }
 }
 
+const gameboard = new Gameboard();
+console.log(gameboard.board);
+console.log(gameboard.enemyBoard);
+*/
 const ship = new Ship(3);
 ship.hit();
 ship.isSunk();
 
 ship;
 
-module.exports = { Ship };
+module.exports = Ship;
---
+++ test/shipt.test.js
@@ -0,0 +1,23 @@
+const Ship = require("../src/ship");
+
+let testShip = new Ship(2);
+
+test("checks if test ship has proper length", () => {
+  expect(testShip.length).toBe(2);
+});
+
+test("adds 1 to hit count", () => {
+  testShip.hit();
+  expect(testShip.hits).toBe(1);
+});
+
+test("tests if ship recognizes it is not sunk", () => {
+  testShip.isSunk();
+  expect(testShip.sunk).toBe(false);
+});
+
+test("tests if ship recognizes it is sunk", () => {
+  testShip.hit();
+  testShip.isSunk();
+  expect(testShip.sunk).toBe(true);
+});
